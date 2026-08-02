4a15feca2d30b83beb6b0a71741b506b85cc23a2
nabilfattabarani-coder <nabilfattabarani@gmail.com>
2026-08-02 12:40:55 +0800

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,5 +1,5 @@
 export default async function handler(req, res) {
-  res.setHeader('Access-Control-Allow-Origin', 'https://sman10gowa.vercel.app');
+  res.setHeader('Access-Control-Allow-Origin', 'https://biosinc-ai.vercel.app');
   res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
   res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
 
