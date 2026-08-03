68838a362494e0b6d247637a247227376624e704
nabilfattabarani-coder <nabilfattabarani@gmail.com>
2026-08-03 08:35:55 +0800

    Update search.js

--- api/search.js
+++ api/search.js
@@ -1,4 +1,8 @@
 export default async function handler(req, res) {
+  res.setHeader('Access-Control-Allow-Origin', '*');
+  res.setHeader('Access-Control-Allow-Methods', 'POST, OPTIONS');
+  res.setHeader('Access-Control-Allow-Headers', 'Content-Type');
+
   if (req.method !== 'POST') {
     return res.status(405).json({ error: 'Method not allowed' });
   }
